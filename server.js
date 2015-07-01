537bab878f964848db1f6c21bbe81ff327f2fc5f
adam_dicken <adam.dicken@totaljobsgroup.com>
2015-07-01 11:31:59 +0000

    added basic /images/{item} endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var ConnectSdk = require("connectsdk");
  
 var server = restify.createServer({
   name: 'myapp',
@@ -11,6 +12,29 @@ server.use(restify.bodyParser());
 server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
   return next();
+});
+
+server.get('/images/:q', function(req, res, next){
+  var connectSdk = new ConnectSdk (
+    process.env.ConnectSDK_ApiKey,
+    process.env.ConnectSDK_ApiSecret,
+    process.env.ConnectSDK_UserName,
+    process.env.ConnectSDK_UserPassword);
+    
+    var search = connectSdk
+    .search()
+    .images()
+    .withPage(1)
+    .withPageSize(1)
+    .withPhrase(req.params.q);
+    
+    search.execute(function(err, response) {
+      if (err) throw err
+      res.send(JSON.stringify(response.images[0]))
+    });
+    
+    return next();
+
 });
  
 server.listen(process.env.PORT || 8080, function () {
